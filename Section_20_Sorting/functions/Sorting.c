fb6e3287d6f3213c40ca8389594579bd6cef0750
Nicolas Saavedra <njsr27@gmail.com>
2021-01-11 01:28:39 -0300

    Added quick sort

--- Section_20_Sorting/functions/Sorting.c
+++ Section_20_Sorting/functions/Sorting.c
@@ -65,3 +65,31 @@ void List_Sort_Selection(int list[], int length) {
     printf("\nNumber of comparisons %d\n", comp);
     printf("Number of swaps %d\n", swap);
 }
+
+void List_Sort_Quick(int list[], int l, int h) {
+    int i, j, pivot, temp;
+
+    if (l < h) {
+        pivot = l;
+        i = l;
+        j = h;
+
+        while (i < j) {
+            while (list[i] <= list[pivot] && i < h)
+                i++;
+            while (list[j] > list[pivot])
+                j--;
+            if (i < j) {
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+        }
+
+        temp = list[pivot];
+        list[pivot] = list[j];
+        list[j] = temp;
+        List_Sort_Quick(list, l, j - 1);
+        List_Sort_Quick(list, j + 1, h);
+    }
+}
--- Section_20_Sorting/main.c
+++ Section_20_Sorting/main.c
@@ -1,6 +1,7 @@
 //#include "tests/Bubble_Sort_Tests.c"
 //#include "tests/Insertion_Sort_Tests.c"
-#include "tests/Selection_Sort_Tests.c"
+//#include "tests/Selection_Sort_Tests.c"
+#include "tests/Quick_Sort_Tests.c"
 
 /**
  * Algorithms
@@ -23,7 +24,8 @@
 int main() {
     //Bubble_Sort_Test();
     //Insertion_Sort_Test();
-    Selection_Sort_Test();
+    //Selection_Sort_Test();
+    Quick_Sort_Test();
     return 0;
 }
 
